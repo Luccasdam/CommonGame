81cf7284177f108e5409cc89a73e885fc9f2b483
Luccasdam <luccas_crims@hotmail.com>
2026-05-30 12:05:44 -0400

    Add base GameMode and GameState

---
+++ Source/CommonGame/Game/CG_GameMode.h
@@ -0,0 +1,16 @@
+﻿// Copyright Luccas de Avila Machado. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CG_GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract)
+class COMMONGAME_API ACG_GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+};
---
+++ Source/CommonGame/Game/CG_GameState.h
@@ -0,0 +1,16 @@
+﻿// Copyright Luccas de Avila Machado. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "CG_GameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract)
+class COMMONGAME_API ACG_GameState : public AGameStateBase
+{
+	GENERATED_BODY()
+};
