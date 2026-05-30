333a7a153462b3ef6b271a8708a450e65a2c92fc
Luccasdam <luccas_crims@hotmail.com>
2026-05-30 13:01:22 -0400

    Add gameplaytags for the UserInterface Layers

---
+++ Source/CommonGame/UI/Statics/CG_UI_GameplayTags.cpp
@@ -0,0 +1,22 @@
+﻿// Copyright Luccas de Avila Machado. All rights reserved.
+
+
+#include "CG_UI_GameplayTags.h"
+
+
+namespace CG
+{
+	namespace GameplayTags
+	{
+		namespace UI
+		{
+			namespace Layer
+			{
+				UE_DEFINE_GAMEPLAY_TAG(Modal, "UI.Layer.Modal")
+				UE_DEFINE_GAMEPLAY_TAG(GameMenu, "UI.Layer.GameMenu")
+				UE_DEFINE_GAMEPLAY_TAG(GameHUD, "UI.Layer.GameHUD")
+				UE_DEFINE_GAMEPLAY_TAG(Frontend, "UI.Layer.Frontend")
+			}
+		}
+	}
+}
---
+++ Source/CommonGame/UI/Statics/CG_UI_GameplayTags.h
@@ -0,0 +1,23 @@
+﻿// Copyright Luccas de Avila Machado. All rights reserved.
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+
+namespace CG
+{
+	namespace GameplayTags
+	{
+		namespace UI
+		{
+			namespace Layer
+			{
+				COMMONGAME_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Modal)
+				COMMONGAME_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(GameMenu)
+				COMMONGAME_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(GameHUD)
+				COMMONGAME_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend)
+			}
+		}
+	}
+}
